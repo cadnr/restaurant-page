4e98d8c628e8066c4ff065c6374404f40c64ea1a
nighfy <82936194+nighfy@users.noreply.github.com>
2021-11-29 13:17:32 +0100

    Change nav links names and main text content

--- src/firstLoad.js
+++ src/firstLoad.js
@@ -11,11 +11,11 @@ const firstLoad = () => {
     const nav = document.createElement('nav');
     const ul = document.createElement('ul');
     const a1 = document.createElement('a');
-    a1.textContent = 'link 1';
+    a1.textContent = 'Home';
     const a2 = document.createElement('a');
-    a2.textContent = 'link 2';
+    a2.textContent = 'Menu';
     const a3 = document.createElement('a');
-    a3.textContent = 'link 3';
+    a3.textContent = 'Contact';
     ul.append(a1, a2, a3);
     nav.append(ul);
     header.append(h1, nav);
@@ -27,7 +27,7 @@ const firstLoad = () => {
     const h2 = document.createElement('h2');
     h2.textContent = 'Welcome to The Best Restaurant';
     const div2 = document.createElement('div');
-    div2.textContent = 'We use only fresh ingredients, our cook is the best in the world and we are the cheapest at the same time.'
+    div2.textContent = 'We use only fresh ingredients, our cook is the best in the world and we are the cheapest at the same time. We are the best at everything.'
         + ' Come and try our food on the first day opening of the restaurant!';
     const div3 = document.createElement('div');
     div3.textContent = '50% OFF TODAY!';
